6eab81ffad68e48ac0b22fc71b475c496ea455ae
arash.pourrahim <arash.pourrahim@yahoo.com>
2025-05-22 00:23:49 +0330

    uninstall file-saver

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack(config) {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      buffer: false,
-      ieee754: false,
-      "file-saver": false,
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
