b30d60412c2242711c93a41b31e8b62e1dc87b0e
arash.pourrahim <arash.pourrahim@yahoo.com>
2025-05-21 23:24:06 +0330

    inital commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      buffer: false,
+      ieee754: false,
+      "file-saver": false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
